b1a8125e93749dbfde8f77c0d9426e4c35d9b0c1
RachidLaasri <rashidlaasri@gmail.com>
2015-09-09 20:30:19 +0100

    Update messages

--- src/Lang/en/messages.php
+++ src/Lang/en/messages.php
@@ -2,50 +2,54 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Shared translations
-    |--------------------------------------------------------------------------
-    */
+    /**
+     *
+     * Shared translations.
+     *
+     */
     'title' => 'Laravel Installer',
     'next' => 'Next Step',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Home page translations
-    |--------------------------------------------------------------------------
-    */
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
     'welcome' => [
         'title'   => 'Welcome To The Installer...',
-        'message' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        'message' => 'Welcome to the setup wizard, please make sure you edited the .env file before clicking next.'
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Requirements page translations
-    |--------------------------------------------------------------------------
-    */
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
     'requirements' => [
         'title' => 'Requirements'
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Permissions page translations
-    |--------------------------------------------------------------------------
-    */
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
     'permissions' => [
         'title' => 'Permissions'
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Installer page translations
-    |--------------------------------------------------------------------------
-    */
+
+    /**
+     *
+     * Database Installer page translations.
+     *
+     */
     'database' => [
         'title' => 'Database Installer',
-        'success' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'success' => 'Congratulations! The script has been installed and ready to be used.',
         'final' => 'Home page'
     ]
 ];
