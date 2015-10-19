284b6b9c7b09bd403726b577766618f5d3ee8195
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-10-20 02:28:21 +0300

    added Romanian translation

---
+++ src/Lang/ro/messages.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+    'title' => 'Procesul de instalare Laravel',
+    'next' => 'Pasul următor',
+
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+    'welcome' => [
+        'title'   => 'Bun venit în procesul de instalare...',
+        'message' => 'Bun venit în configurarea asistată.',
+    ],
+
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+    'requirements' => [
+        'title' => 'Cerințe',
+    ],
+
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+    'permissions' => [
+        'title' => 'Permisiuni',
+    ],
+
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+    'environment' => [
+        'title' => 'Settări ale mediului',
+        'save' => 'Salvează fișier .env',
+        'success' => 'Setările tale au fost salvate în fișierul .env.',
+        'errors' => 'Nu am putut salva fișierul .env, Te rugăm să-l creezi manual.',
+    ],
+
+
+    /**
+     *
+     * upgrade welcome page translations.
+     *
+     */
+    'upgrade' => [
+        'title' => 'Actualizează aplicația',
+        'welcome' => 'Folosiți momentan versiunea :current iar cea mai nouă versiune disponibilă este :latest. Click pentru actualizare.',
+        'button' => 'Actualizează',
+    ],
+
+
+    /**
+     *
+     * Final page translations.
+     *
+     */
+    'final' => [
+        'title' => 'Am terminat',
+        'finished' => 'Aplicația a fost instalată cu succes.',
+        'exit' => 'Click aici pentru a ieși',
+    ],
+];
