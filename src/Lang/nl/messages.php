ccbfb4e1e5a5287b944ad1d738269c3800c12963
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-11-23 22:10:40 +0100

    Add Dutch translation

---
+++ src/Lang/nl/messages.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+    'title' => 'Laravel Installer',
+    'next' => 'Volgende stap',
+
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+    'welcome' => [
+        'title'   => 'Welkom bij het installatie proces...',
+        'message' => 'Welkom bij de installatiewizard',
+    ],
+
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+    'requirements' => [
+        'title' => 'Vereisten',
+    ],
+
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+    'permissions' => [
+        'title' => 'Permissies',
+    ],
+
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+    'environment' => [
+        'title' => 'Environment Settings',
+        'save' => '.env Opslaan',
+        'success' => 'Uw .env bestand is opgeslagen.',
+        'errors' => 'Het is niet mogelijk om een .env bestand aan te maken, maak a.u.b het bestand zelf aan.',
+    ],
+
+
+    /**
+     *
+     * upgrade welcome page translations.
+     *
+     */
+    'upgrade' => [
+        'title' => 'Upgrade applicatie',
+        'welcome' => 'Uw draait op het moment versie :current and de laatst beschikbare versie is :latest klik om te upgraden.',
+        'button' => 'Upgrade',
+    ],
+
+
+    /**
+     *
+     * Final page translations.
+     *
+     */
+    'final' => [
+        'title' => 'Voltooid',
+        'finished' => 'Applicatie is succesvol geïnstalleerd.',
+        'exit' => 'Klik hier om af te sluiten.',
+    ],
+];
