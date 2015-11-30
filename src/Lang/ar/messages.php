e549efbd95104fb72648b94d4ea46b9c03b836a9
RachidLaasri <rashidlaasri@gmail.com>
2015-11-30 12:19:55 +0000

    Add Arabic support

---
+++ src/Lang/ar/messages.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+    'title' => 'تنصيب Laravel',
+    'next' => 'متابعة',
+
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+    'welcome' => [
+        'title'   => 'تنصيب Laravel',
+        'message' => 'أهلا بك في صفحة تنصيب Laravel',
+    ],
+
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+    'requirements' => [
+        'title' => 'المتطلبات',
+    ],
+
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+    'permissions' => [
+        'title' => 'تراخيص المجلدات',
+    ],
+
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+    'environment' => [
+        'title' => 'الإعدادات',
+        'save' => 'حفظ ملف .env',
+        'success' => 'تم حفظ الإعدادات بنجاح',
+        'errors' => 'حدث خطأ أثناء إنشاء ملف .env. رجاءا قم بإنشاءه يدويا',
+    ],
+
+
+    /**
+     *
+     * upgrade welcome page translations.
+     *
+     */
+    'upgrade' => [
+        'title' => 'Upgrade application',
+        'welcome' => 'Your are currently running version :current and the latest available version is :latest click to upgrade.',
+        'button' => 'Upgrade',
+    ],
+
+
+    /**
+     *
+     * Final page translations.
+     *
+     */
+    'final' => [
+        'title' => 'النهاية',
+        'finished' => 'تم تنصيب البرنامج بنجاح...',
+        'exit' => 'إضغط هنا للخروج',
+    ],
+];
