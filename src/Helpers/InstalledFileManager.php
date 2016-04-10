659a5b3d124f6c2af2369612e6b433f2df0db5c7
Vignesh <pvignesh90@gmail.com>
2016-04-11 01:20:05 +0530

    semi colon issue - Vignesh

--- src/Helpers/InstalledFileManager.php
+++ src/Helpers/InstalledFileManager.php
@@ -15,7 +15,7 @@ class InstalledFileManager
         file_put_contents(storage_path('installed'), '');
 
         $result = file_get_contents(base_path('.env'));
-        $newLine = "APP_ENV=staging\nSESSION_DRIVER=database\n"
+        $newLine = "APP_ENV=staging\nSESSION_DRIVER=database\n";
         fwrite($result, $newLine);
     }
 
