ec35fe92b00016f2c2dff098f047b486544cc7eb
newsmodulesettings <jaap@novisites.nl>
2016-06-08 08:59:41 +0200

    Textarea to textfields
    Set the textarea to textfield

--- src/Controllers/EnvironmentController.php
+++ src/Controllers/EnvironmentController.php
@@ -2,8 +2,12 @@
 
 namespace RachidLaasri\LaravelInstaller\Controllers;
 
-use Illuminate\Routing\Controller;
+use App, PDOException, error;
+
+
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Routing\Redirector;
 use RachidLaasri\LaravelInstaller\Helpers\EnvironmentManager;
 
@@ -32,7 +36,49 @@ class EnvironmentController extends Controller
     {
         $envConfig = $this->EnvironmentManager->getEnvContent();
 
-        return view('vendor.installer.environment', compact('envConfig'));
+        //Sets the default values of the .env file!
+
+        $APP_ENV = 'local';
+        $APP_DEBUG = 'true';
+        $APP_KEY =  '4958763498756837942368weufgsdgw3';
+
+        $DB_HOST = 'localhost';
+        $DB_DATABASE = 'database';
+        $DB_USERNAME = 'Username';
+        $DB_PASSWORD = 'Password';
+        
+        $CACHE_DRIVER = 'file';
+        $SESSION_DRIVER = 'file';
+        $QUEUE_DRIVER = 'sync';
+
+        $MAIL_DRIVER = 'smtp';
+        $MAIL_HOST = 'mailtrap.io';
+        $MAIL_PORT= 2525;
+        $MAIL_USERNAME = 'null';
+        $MAIL_PASSWORD = 'null';
+        $MAIL_ENCRYPTION= 'null';
+
+
+        return view('vendor.installer.environment', 
+            [
+                'APP_ENV' => $APP_ENV,
+                'APP_DEBUG' => $APP_DEBUG,
+                'APP_KEY' => $APP_KEY,
+                'DB_HOST' => $DB_HOST,
+                'DB_DATABASE' => $DB_DATABASE,
+                'DB_USERNAME' => $DB_USERNAME,
+                'DB_PASSWORD' => $DB_PASSWORD,
+                'CACHE_DRIVER' => $CACHE_DRIVER,
+                'SESSION_DRIVER' => $SESSION_DRIVER,
+                'QUEUE_DRIVER' => $QUEUE_DRIVER,
+                'MAIL_DRIVER' => $MAIL_DRIVER,
+                'MAIL_HOST' => $MAIL_HOST,
+                'MAIL_PORT' => $MAIL_PORT,
+                'MAIL_USERNAME' => $MAIL_USERNAME,
+                'MAIL_PASSWORD' => $MAIL_PASSWORD,
+                'MAIL_ENCRYPTION' => $MAIL_ENCRYPTION,
+            ]
+        );
     }
 
 
@@ -44,11 +90,57 @@ class EnvironmentController extends Controller
      * @return \Illuminate\Http\RedirectResponse
      */
     public function save(Request $input, Redirector $redirect)
-    {
-        $message = $this->EnvironmentManager->saveFile($input);
+    {        
+$file = "APP_ENV=".$input->APP_ENV."
+APP_DEBUG=".$input->APP_DEBUG."
+APP_KEY=".$input->APP_KEY."
 
-        return $redirect->route('LaravelInstaller::environment')
-                        ->with(['message' => $message]);
+DB_HOST=".$input->DB_HOST."
+DB_DATABASE=".$input->DB_DATABASE."
+DB_USERNAME=".$input->DB_USERNAME."
+DB_PASSWORD=".$input->DB_PASSWORD."
+
+CACHE_DRIVER=".$input->CACHE_DRIVER."
+SESSION_DRIVER=".$input->SESSION_DRIVER."
+QUEUE_DRIVER=".$input->QUEUE_DRIVER."
+
+MAIL_DRIVER=".$input->MAIL_DRIVER."
+MAIL_HOST=".$input->MAIL_HOST."
+MAIL_PORT=".$input->MAIL_PORT."
+MAIL_USERNAME=".$input->MAIL_USERNAME."
+MAIL_PASSWORD=".$input->MAIL_PASSWORD."
+MAIL_ENCRYPTION=".$input->MAIL_ENCRYPTION."
+";
+
+
+        $message = 'Successfully saved';
+
+        $myfile = fopen("../.env", "w") or die("Unable to open file!");
+        $txt = $file;
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+        return view('vendor.installer.environment', 
+            [
+                'message' => $message,
+                'APP_ENV' => $input->APP_ENV,
+                'APP_DEBUG' => $input->APP_DEBUG,
+                'APP_KEY' => $input->APP_KEY,
+                'DB_HOST' => $input->DB_HOST,
+                'DB_DATABASE' => $input->DB_DATABASE,
+                'DB_USERNAME' => $input->DB_USERNAME,
+                'DB_PASSWORD' => $input->DB_PASSWORD,
+                'CACHE_DRIVER' => $input->CACHE_DRIVER,
+                'SESSION_DRIVER' => $input->SESSION_DRIVER,
+                'QUEUE_DRIVER' => $input->QUEUE_DRIVER,
+                'MAIL_DRIVER' => $input->MAIL_DRIVER,
+                'MAIL_HOST' => $input->MAIL_HOST,
+                'MAIL_PORT' => $input->MAIL_PORT,
+                'MAIL_USERNAME' => $input->MAIL_USERNAME,
+                'MAIL_PASSWORD' => $input->MAIL_PASSWORD,
+                'MAIL_ENCRYPTION' => $input->MAIL_ENCRYPTION,
+            ]
+        );
     }
 
 }
--- src/Views/environment.blade.php
+++ src/Views/environment.blade.php
@@ -1,12 +1,97 @@
 @extends('vendor.installer.layouts.master')
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
 @section('title', trans('messages.environment.title'))
 @section('container')
-    @if (session('message'))
-    <p class="alert">{{ session('message') }}</p>
+    @if (session('old'))
+    <p class="alert">{{ session('old') }}</p>
     @endif
     <form method="post" action="{{ route('LaravelInstaller::environmentSave') }}">
-        <textarea class="textarea" name="envConfig">{{ $envConfig }}</textarea>
+       
+
+        <div class="form-group">
+            <label for="APP_ENV">APP_ENV</label>
+            <input type="text" class="form-control" name="APP_ENV" value="{{$APP_ENV}}">
+        </div>
+          
+        <div class="form-group">
+            <label for="APP_DEBUG">APP_DEBUG</label>
+            <input type="text" class="form-control" name="APP_DEBUG" value="{{$APP_DEBUG}}">
+        </div>
+
+        <div class="form-group">
+            <label for="APP_KEY">APP_KEY</label>
+            <input type="text" class="form-control" name="APP_KEY" value="{{$APP_KEY}}">
+        </div>
+        <hr></hr>
+        <div class="form-group">
+            <label for="DB_HOST">DB_HOST</label>
+            <input type="text" class="form-control" name="DB_HOST" value="{{$DB_HOST}}">
+        </div>
+
+        <div class="form-group">
+            <label for="DB_DATABASE">DB_DATABASE</label>
+            <input type="text" class="form-control" name="DB_DATABASE" value="{{$DB_DATABASE}}">
+        </div>
+
+        <div class="form-group">
+            <label for="DB_USERNAME">DB_USERNAME</label>
+            <input type="text" class="form-control" name="DB_USERNAME" value="{{$DB_USERNAME}}">
+        </div>
+
+        <div class="form-group">
+            <label for="DB_PASSWORD">DB_PASSWORD</label>
+            <input type="text" class="form-control" name="DB_PASSWORD" value="{{$DB_PASSWORD}}">
+        </div>
+        <hr></hr>
+        <div class="form-group">
+            <label for="CACHE_DRIVER">CACHE_DRIVER</label>
+            <input type="text" class="form-control" name="CACHE_DRIVER" value="{{$CACHE_DRIVER}}">
+        </div>
+
+        <div class="form-group">
+            <label for="SESSION_DRIVER">SESSION_DRIVER</label>
+            <input type="text" class="form-control" name="SESSION_DRIVER" value="{{$SESSION_DRIVER}}">
+        </div>
+
+        <div class="form-group">
+            <label for="QUEUE_DRIVER">QUEUE_DRIVER</label>
+            <input type="text" class="form-control" name="QUEUE_DRIVER" value="{{$QUEUE_DRIVER}}">
+        </div>
+        <hr></hr>
+        <div class="form-group">
+            <label for="MAIL_DRIVER">MAIL_DRIVER</label>
+            <input type="text" class="form-control" name="MAIL_DRIVER" value="{{$MAIL_DRIVER}}">
+        </div>
+
+        <div class="form-group">
+            <label for="MAIL_HOST">MAIL_HOST</label>
+            <input type="text" class="form-control" name="MAIL_HOST" value="{{$MAIL_HOST}}">
+        </div>
+
+        <div class="form-group">
+            <label for="MAIL_PORT">MAIL_PORT</label>
+            <input type="text" class="form-control" name="MAIL_PORT" value="{{$MAIL_PORT}}">
+        </div>
+
+        <div class="form-group">
+            <label for="MAIL_USERNAME">MAIL_USERNAME</label>
+            <input type="text" class="form-control" name="MAIL_USERNAME" value="{{$MAIL_USERNAME}}">
+        </div>
+
+        <div class="form-group">
+            <label for="MAIL_PASSWORD">MAIL_PASSWORD</label>
+            <input type="text" class="form-control" name="MAIL_PASSWORD" value="{{$MAIL_PASSWORD}}">
+        </div>
+
+        <div class="form-group">
+            <label for="MAIL_ENCRYPTION">MAIL_ENCRYPTION</label>
+            <input type="text" class="form-control" name="MAIL_ENCRYPTION" value="{{$MAIL_ENCRYPTION}}">
+        </div>
+
+
+       
+
+
         {!! csrf_field() !!}
         <div class="buttons buttons--right">
              <button class="button button--light" type="submit">{{ trans('messages.environment.save') }}</button>
