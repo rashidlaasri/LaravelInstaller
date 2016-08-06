c539356721f59f8202b506673b06e23a26688a2b
raywill <hustos@qq.com>
2016-08-06 15:12:20 +0800

    add Simplified Chinese support

---
+++ src/Lang/zh/messages.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+    'title' => '安装向导',
+    'next' => '下一步',
+    'install' => '安装',
+    'finish' => '完成',
+
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+    'welcome' => [
+        'title'   => '安装',
+        'message' => '欢迎使用',
+    ],
+
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+    'requirements' => [
+        'title' => '安装环境检测 - PHP 插件',
+    ],
+
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+    'permissions' => [
+        'title' => '安装环境检测 - 目录权限',
+    ],
+
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+    'environment' => [
+        'title' => '参数设置',
+        'save' => '保存 .env',
+        'success' => '.env 文件保存成功.',
+        'errors' => '.env 文件保存失败，请手工创建',
+    ],
+
+
+    /**
+     *
+     * Final page translations.
+     *
+     */
+    'final' => [
+        'title' => '安装成功',
+        'finished' => '感谢使用',
+        'exit' => '完成',
+    ],
+];
