f8baa815b899d1128fbf05aaa48b86e4a06b9b6e
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-10-31 10:26:00 +0000

    Updating the views

--- src/Views/environment.blade.php
+++ src/Views/environment.blade.php
@@ -3,20 +3,20 @@
 @section('title', trans('messages.environment.title'))
 @section('container')
     @if (session('message'))
-    <p class="alert">{{ session('message') }}</p>
+        <p class="alert">{{ session('message') }}</p>
     @endif
     <form method="post" action="{{ route('LaravelInstaller::environmentSave') }}">
-        <textarea class="textarea" name="envConfig">{{ $envConfig }}</textarea>
         {!! csrf_field() !!}
+        <textarea class="textarea" name="envConfig">{{ $envConfig }}</textarea>
         <div class="buttons buttons--right">
              <button class="button button--light" type="submit">{{ trans('messages.environment.save') }}</button>
         </div>
     </form>
-    @if(!isset($environment['errors']))
-    <div class="buttons">
-        <a class="button" href="{{ route('LaravelInstaller::requirements') }}">
-            {{ trans('messages.next') }}
-        </a>
-    </div>
+    @if( ! isset($environment['errors']))
+        <div class="buttons">
+            <a class="button" href="{{ route('LaravelInstaller::requirements') }}">
+                {{ trans('messages.next') }}
+            </a>
+        </div>
     @endif
 @stop
--- src/Views/requirements.blade.php
+++ src/Views/requirements.blade.php
@@ -2,19 +2,17 @@
 
 @section('title', trans('messages.requirements.title'))
 @section('container')
+    <ul class="list">
+        @foreach($requirements['requirements'] as $extention => $enabled)
+        <li class="list__item {{ $enabled ? 'success' : 'error' }}">{{ $extention }}</li>
+        @endforeach
+    </ul>
 
-<ul class="list">
-    @foreach($requirements['requirements'] as $extention => $enabled)
-    <li class="list__item @if($enabled) success @else error @endif">{{ $extention }}</li>
-    @endforeach
-</ul>
-
-@if(!isset($requirements['errors']))
-    <div class="buttons">
-        <a class="button" href="{{ route('LaravelInstaller::permissions') }}">
-        {{ trans('messages.next') }}
-        </a>
-    </div>
-@endif
-
+    @if ( ! isset($requirements['errors']))
+        <div class="buttons">
+            <a class="button" href="{{ route('LaravelInstaller::permissions') }}">
+                {{ trans('messages.next') }}
+            </a>
+        </div>
+    @endif
 @stop
