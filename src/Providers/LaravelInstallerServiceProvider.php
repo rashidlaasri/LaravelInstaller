1f68792828cbe10cdaf2833b5b2a57708750a456
Juan Manuel Verges Solanas <juanmanuel.vergessolanas@gmail.com>
2017-08-03 21:51:28 +0200

    [TASK] Update provider to reflect new paths

--- src/Providers/LaravelInstallerServiceProvider.php
+++ src/Providers/LaravelInstallerServiceProvider.php
@@ -24,7 +24,7 @@ class LaravelInstallerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->publishFiles();
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
     }
 
     /**
@@ -46,19 +46,19 @@ class LaravelInstallerServiceProvider extends ServiceProvider
     protected function publishFiles()
     {
         $this->publishes([
-            __DIR__.'/../Config/installer.php' => base_path('config/installer.php'),
+            __DIR__.'/../../config/installer.php' => base_path('config/installer.php'),
         ], 'laravelinstaller');
 
         $this->publishes([
-            __DIR__.'/../assets' => public_path('installer'),
+            __DIR__.'/../../resources/assets' => public_path('installer'),
         ], 'laravelinstaller');
 
         $this->publishes([
-            __DIR__.'/../Views' => base_path('resources/views/vendor/installer'),
+            __DIR__.'/../../resources/views' => base_path('resources/views/vendor/installer'),
         ], 'laravelinstaller');
 
         $this->publishes([
-            __DIR__.'/../Lang' => base_path('resources/lang'),
+            __DIR__.'/../../resources/lang' => base_path('resources/lang'),
         ], 'laravelinstaller');
     }
 }
