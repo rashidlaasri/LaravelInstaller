7675aa30e6f5b3018fe4483b0169136ccd063116
divyanshukr96 <divyanshu461@gmail.com>
2019-06-16 15:32:06 +0530

    LogicException Error with Laravel removed

--- src/Config/installer.php
+++ src/Config/installer.php
@@ -65,10 +65,7 @@ return [
                 'app_name'              => 'required|string|max:50',
                 'environment'           => 'required|string|max:50',
                 'environment_custom'    => 'required_if:environment,other|max:50',
-                'app_debug'             => [
-                    'required',
-                    Rule::in(['true', 'false']),
-                ],
+                'app_debug'             => 'required|boolean',
                 'app_log_level'         => 'required|string|max:50',
                 'app_url'               => 'required|url',
                 'database_connection'   => 'required|string|max:50',
