606b28f9aadaa3040bfcbbf2aaaeead2cd2c1072
Kirill Uksusov <uksusoff@yandex.ru>
2019-08-12 21:47:33 +0400

    TODO removed
    TODO from internal project removed. Sorry for this.

--- src/Controllers/EnvironmentController.php
+++ src/Controllers/EnvironmentController.php
@@ -112,7 +112,6 @@ class EnvironmentController extends Controller
     }
 
     /**
-     * TODO: We can remove this code if PR will be merged: https://github.com/RachidLaasri/LaravelInstaller/pull/162
      * Validate database connection with user credentials (Form Wizard).
      *
      * @param Request $request
