a0a49ed41a868faecb693b87f0a8a90b454d145e
Rizwan <rkbwp@yahoo.com>
2020-02-23 16:30:35 +0500

    Str FIX

--- src/Helpers/EnvironmentManager.php
+++ src/Helpers/EnvironmentManager.php
@@ -96,7 +96,7 @@ class EnvironmentManager
         $envFileData =
         'APP_NAME=\'' . $request->app_name . "'\n" .
         'APP_ENV=' . $request->environment . "\n" .
-        'APP_KEY=' .base64_encode(Str::random(32)). "\n" .
+        'APP_KEY=base64:' .base64_encode(Str::random(32)). "\n" .
         'APP_DEBUG=' . $request->app_debug . "\n" .
         'APP_LOG_LEVEL=' . $request->app_log_level . "\n" .
         'APP_URL=' . $request->app_url . "\n\n" .
